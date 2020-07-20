ef5cba90d740f263fb656b0de63abd19b49a2caf
Jake Armendariz <jakearmendariz99@gmail.com>
2020-07-20 02:40:39 -0700

    first commit -m

---
+++ NumberBase.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class AnyBase {
+
+	// Handles larger numbers
+	public static long[] getDigits(long N, long b) {
+		long power = 0;
+		while (Math.pow(b, power) <= N) {
+			power++;
+		}
+
+		long[] result = new long[(int)power];
+		while (N > 0 && power > 0) {
+			power--;
+			long c = 0;
+			while (c * Math.pow(b, power) <= N) {
+				c++;
+			}
+			c--;
+			N = (long) (N - (c * Math.pow(b, power)));
+			result[(int)power] = c;
+		}
+		return result;
+	}
+	
+	
+	//This is a basic UI to allow running of the program
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		long b;
+		System.out.println("Enter any large number under 9,223,372,036,854,775,807");
+		long a = in.nextLong();
+		System.out.println("Enter any number smaller than the first and 2,147,483,647");
+		b = in.nextLong();
+		long arr[] = getDigits(a, b);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println("\n");
+		
+		long[] ar = new long[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			long x = (long) (arr[i] * Math.pow(b, i));
+			ar[i] = x;
+			System.out.println(arr[i] + " * " + b + " ^ " + i + " = " + x);
+			//System.out.prlongln(Math.pow(b, i));
+		}
+		System.out.println("");
+		long sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(ar[i] + " + ");
+			sum += ar[i];
+		}
+		System.out.println("= " + a);
+	}
+
+}
---
+++ NumberBase.py
@@ -0,0 +1,54 @@
+import math
+
+def createNumberSystem(translator):  # 62 characters
+    for i in range(0, 10):
+        translator.append(str(i))    # 1- 10
+    for i in range(0, 26):
+        translator.append(chr(97+i)) # a -z
+    for i in range(0, 26):
+        translator.append(chr(65+i)) # A - z
+
+def addSpecialCharacters(translator):# 15 charaters.. 77 overall
+    for i in range(33, 48):
+        translator.append(chr(i))
+    for i in range(58, 41):
+        translator.append(chr(i))
+
+def numberBase(translator, number, base):
+    power = getOutputLength(number, base)
+    result = ''
+    while(power > -1):
+        digit = math.pow(base, power)
+        hiConstant = highestConstant(number, digit, base)
+        print(base,"^", power, "*", hiConstant,"\t=", digit*hiConstant)
+        if(hiConstant <= number):
+            number -= int(digit*hiConstant)
+            result += translator[hiConstant]
+        else:
+            digit = 0
+            result += '0'
+        power -= 1
+    return result
+
+def highestConstant(number, digit, base):
+    for i in range(1, base):
+        if(digit*i > number):
+            return i-1
+        if(digit*i == number):
+            return i
+    return base -1
+
+
+def getOutputLength(number, base):
+    power = 0
+    while(math.pow(base, power) < number):
+        power+=1
+    return power-1
+
+translator = []
+createNumberSystem(translator)
+addSpecialCharacters(translator)
+print('max-base size:', len(translator))
+N = input("Enter a number: ")
+B = input("Enter a base: ")
+print(numberBase(translator, int(N), int(B)))
